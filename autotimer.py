dd7905f8915e945e5d3cf25082492c0c143aa5f3
Amit JS <amitjslearn@gmail.com>
2019-08-27 02:16:12 +0530

    to support linux
    To support Linux additional snippets are added.

--- autotimer.py
+++ autotimer.py
@@ -11,16 +11,21 @@ if sys.platform in ['Windows', 'win32', 'cygwin']:
 elif sys.platform in ['Mac', 'darwin', 'os2', 'os2emx']:
     from AppKit import NSWorkspace
     from Foundation import *
+elif sys.platform in ['linux', 'linux2']:
+        import linux as l
 
 active_window_name = ""
 activity_name = ""
 start_time = datetime.datetime.now()
-activeList = AcitivyList([]) 
+activeList = AcitivyList([])
 first_time = True
+
 
 def url_to_name(url):
     string_list = url.split('/')
     return string_list[2]
+
+
 def get_active_window():
     _active_window_name = None
     if sys.platform in ['Windows', 'win32', 'cygwin']:
@@ -28,21 +33,24 @@ def get_active_window():
         _active_window_name = win32gui.GetWindowText(window)
     elif sys.platform in ['Mac', 'darwin', 'os2', 'os2emx']:
         _active_window_name = (NSWorkspace.sharedWorkspace()
-                              .activeApplication()['NSApplicationName'])
+                               .activeApplication()['NSApplicationName'])
     else:
         print("sys.platform={platform} is not supported."
               .format(platform=sys.platform))
         print(sys.version)
     return _active_window_name
+
+
 def get_chrome_url():
     if sys.platform in ['Windows', 'win32', 'cygwin']:
-        window = win32gui.GetForegroundWindow() 
+        window = win32gui.GetForegroundWindow()
         chromeControl = auto.ControlFromHandle(window)
         edit = chromeControl.EditControl()
         return 'https://' + edit.GetValuePattern().Value
     elif sys.platform in ['Mac', 'darwin', 'os2', 'os2emx']:
         textOfMyScript = """tell app "google chrome" to get the url of the active tab of window 1"""
-        s = NSAppleScript.initWithSource_(NSAppleScript.alloc(), textOfMyScript)
+        s = NSAppleScript.initWithSource_(
+            NSAppleScript.alloc(), textOfMyScript)
         results, err = s.executeAndReturnError_(None)
         return results.stringValue()
     else:
@@ -60,37 +68,43 @@ except Exception:
 try:
     while True:
         previous_site = ""
-        new_window_name = get_active_window()
+        if sys.platform not in ['linux', 'linux2']:
+            new_window_name = get_active_window()
+            if 'Google Chrome' in new_window_name:
+                new_window_name = url_to_name(get_chrome_url())
+        if sys.platform in ['linux', 'linux2']:
+            new_window_name = l.get_active_window_x()
+            if 'Google Chrome' in new_window_name:
+                new_window_name = l.get_chrome_url_x()
+
         
-        if 'Google Chrome' in new_window_name : 
-            new_window_name = url_to_name(get_chrome_url())
         if active_window_name != new_window_name:
             print(active_window_name)
             activity_name = active_window_name
-            
+
             if not first_time:
                 end_time = datetime.datetime.now()
                 time_entry = TimeEntry(start_time, end_time, 0, 0, 0, 0)
                 time_entry._get_specific_times()
-            
+
                 exists = False
                 for activity in activeList.activities:
                     if activity.name == activity_name:
                         exists = True
                         activity.time_entries.append(time_entry)
-            
+
                 if not exists:
                     activity = Activity(activity_name, [time_entry])
-                    activeList.activities.append(activity)  
-                with open('activities.json', 'w') as json_file:  
-                    json.dump(activeList.serialize(), json_file, indent=4, sort_keys=True)
+                    activeList.activities.append(activity)
+                with open('activities.json', 'w') as json_file:
+                    json.dump(activeList.serialize(), json_file,
+                              indent=4, sort_keys=True)
                     start_time = datetime.datetime.now()
             first_time = False
             active_window_name = new_window_name
 
-
-        time.sleep(10)
+        time.sleep(1)
+    
 except KeyboardInterrupt:
-    with open('activities.json', 'w') as json_file:  
+    with open('activities.json', 'w') as json_file:
         json.dump(activeList.serialize(), json_file, indent=4, sort_keys=True)
-
